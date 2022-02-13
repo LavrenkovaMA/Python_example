1a3cebb51dc9835bde7f71f7bec9d7bfaacc9bc0
marina <lavrenkova_ma@mail.ru>
2022-02-13 15:32:46 +0300

    add"удалили лишнее"

--- ex4.py
+++ ex4.py
@@ -1,12 +1,5 @@
 # #Показать первую цифру дробной части числа
-# num = 1.396
-# for c in str(num):
-#     if c == ".":
-#         check = True
-#     if check:
-#         print(c)
-#         break
-    
+
 # a=1.745
 # print(int(((a*10) % 10)))
 a = 1.8665
