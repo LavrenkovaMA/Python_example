2d3811212e45b326c2efc66e82f7600c71144f18
marina <lavrenkova_ma@mail.ru>
2022-02-13 15:07:56 +0300

    add "решили с помощью цикла for"

---
+++ ex2.py
@@ -0,0 +1,6 @@
+#Найти максимальное из пяти чисел
+
+number_list = []
+for i in range(5):
+    number_list.append(int(input(f"Введтите {i+1} число  ")))
+print(f"Максимальное число тиз введенных: {max(number_list)}")
